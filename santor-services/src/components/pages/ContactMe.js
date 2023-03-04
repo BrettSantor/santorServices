db5661d94e768fcf7fc265f777d3b62352103579
BrettSantor <b.santor27@gmail.com>
2023-03-03 18:42:52 -0800

    fill in pages to test

---
+++ santor-services/src/components/pages/ContactMe.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+// import { validateEmail } from '../../utils/helpers';
+
+
+
+
+
+export default function ConstactMe(){
+    return (
+        <div>
+            <h1>Reach Out To Me</h1>
+          
+        </div>
+    );
+}
---
+++ santor-services/src/components/pages/Portfolio.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Portfolio(){
+    return (
+        <div>
+            <h1>Projects</h1>
+            Project cards go here....
+            {/* <Project/> */}
+        </div>
+    );
+}
