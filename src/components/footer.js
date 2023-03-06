8b49a94a335023485e36b19e6c5773290f1181c0
BrettSantor <b.santor27@gmail.com>
2023-03-05 18:31:30 -0800

    add logo pics

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,14 @@
 import React from "react";
-import logo from '../assets/images/favicon.ico'
+import logo from '../assets/images/linkedin.png'
 import github from '../assets/images/githubicon.png'
+import medium from '../assets/images/medium_logo.png'
 
 export default function Footer() {
     return (
         <footer>
-            <a  href='https://linkedin.com/brettsantor' rel="noopener noreferrer" target='_blank'><img src={logo} alt='LinkedIn Logo'/></a>
+            <a  href='https://www.linkedin.com/in/brettsantor/' rel="noopener noreferrer" target='_blank'><img src={logo} alt='LinkedIn Logo'/></a>
             <a  href='https://github.com/BrettSantor' rel="noopener noreferrer" target='_blank'><img src={github} alt='Github Logo'/></a>
-            <a  href='https://github.com/BrettSantor' rel="noopener noreferrer" target='_blank'><img src={logo} alt='Medium Logo'/></a>
+            <a  href='https://medium.com/@b.santor27' rel="noopener noreferrer" target='_blank'><img src={medium} alt='Medium Logo'/></a>
         </footer>
     );
 };
