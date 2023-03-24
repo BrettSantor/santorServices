55218bc90c7218972d1668361ce22ef829ee0989
BrettSantor <b.santor27@gmail.com>
2023-03-24 14:05:54 -0700

    debug home route

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -12,7 +12,7 @@ export default function PortfolioContainer(){
 
       return (
         <div className='portContainer'>
-          <Router>
+          <Router basename='/'>
           <Header/>
           <Routes>
             <Route path='/'
